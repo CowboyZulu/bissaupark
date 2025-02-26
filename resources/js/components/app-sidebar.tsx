a69ed82e78d19ccdab1fb557c63d5e0ca2ed1ced
Sulayman <sulaymanjallow24@gmail.com>
2025-02-26 17:16:52 +0000

    Update sidebar navigation with new vehicle-related menu items

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Users } from 'lucide-react';
+import { BookOpen, Car, Folder, LayoutGrid, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -23,7 +23,22 @@ const mainNavItems: NavItem[] = [
         url: '/roles',
         icon: Users,
     },
-    
+    {
+        title: 'Vehicle Categories',
+        url: '/vehicle-categories',
+        icon: Users,
+    },
+    {
+        title: 'Drivers',
+        url: '/drivers',
+        icon: Car,
+    },
+    {
+        title: 'Vehicles',
+        url: '/vehicles',
+        icon: Car,
+    },
+
 ];
 
 const footerNavItems: NavItem[] = [
